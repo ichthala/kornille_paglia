aff8da62ab18807f52566dca2910b03335981275
ichthala <alice.mottola@gmail.com>
2015-02-24 23:55:41 -0500

    bugfixes

--- lyrics_scraper.rb
+++ lyrics_scraper.rb
@@ -17,12 +17,16 @@ class LyricsScraper
     f = File.new("#{artist_name}_lyrics.txt", "w+")
 
     doc.css('#listAlbum a[target=_blank]').each do |link|
-      song_title = link.content.downcase.gsub(/\s/, '')
+      song_title = link.content.downcase.gsub(/\s\W/, '')
       puts "Getting song #{link.content}..."
-      lyrics_doc = Nokogiri::HTML(open("http://www.azlyrics.com/#{artist_name}/#{song_title}.html"))
-      lyrics_node = lyrics_doc.at("//comment()[contains(.,'start of lyrics')]")
-      lyrics_node = lyrics_node.parent
-      f.write "#{lyrics_node.content}\n"
+      begin
+        lyrics_doc = Nokogiri::HTML(open("http://www.azlyrics.com/lyrics/#{artist_name}/#{song_title}.html"))
+        lyrics_node = lyrics_doc.at("//comment()[contains(.,'start of lyrics')]")
+        lyrics_node = lyrics_node.parent
+        f.write "#{lyrics_node.content}\n"
+      rescue
+        puts "Something went wrong with the song #{link.content}. Skipping that one..."
+      end
     end
 
     f.close
