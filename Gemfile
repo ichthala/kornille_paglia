53485fc0f009b5b643ba2fcc8e04fc21b42713d9
ichthala <alice.mottola@gmail.com>
2015-02-24 23:25:17 -0500

    that was going really well until they blocked my IP

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+ruby '2.1.4'
+
+gem 'nokogiri'
+gem 'marky_markov'
+gem 'pry'
---
+++ lyrics_scraper.rb
@@ -0,0 +1,31 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class LyricsScraper
+
+  def initialize(artist_name)
+    artist_name = artist_name.downcase.gsub(/\s/, '_')
+    begin
+      puts "http://www.azlyrics.com/#{artist_name[0]}/#{artist_name}.html"
+      doc = Nokogiri::HTML(open("http://www.azlyrics.com/#{artist_name[0]}/#{artist_name}.html"))
+    rescue OpenURI::HTTPError => e
+      puts 'Looks like that artist isn\'t on A-Z Lyrics.'
+      exit(0)
+    end
+
+    doc.css('#listAlbum a[target=_blank]').each do |link|
+      song_title = link.content.downcase.gsub(/\s/, '')
+      puts "Getting song #{link.content}..."
+      lyrics_doc = Nokogiri::HTML(open("http://www.azlyrics.com/#{artist_name}/#{song_title}.html"))
+      lyrics_node = lyrics_doc.at("//comment()[contains(.,'start of lyrics')]")
+      lyrics_node = lyrics_node.parent
+      puts lyrics_node.content
+    end
+  end
+
+end
+
+artist = ARGV[0] || 'Korn'
+LyricsScraper.new(artist)
+
